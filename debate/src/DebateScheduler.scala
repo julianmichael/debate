4615e8bec81ded6da70a94e60f14443b7cbb5349
Julian Michael <julianjohnmichael@gmail.com>
2022-12-24 18:28:02 -0600

    Add informal DebateScheduler spec

---
+++ debate/src/DebateScheduler.scala
@@ -0,0 +1,35 @@
+package debate
+
+object DebateScheduler {
+  case class DebaterLoadConstraint(min: Option[Int], max: Option[Int])
+  case class DebateAssignment(honestDebater: String, dishonestDebater: String, judge: String)
+
+  /**
+     * Produces a list of assignments for a new story.
+     * 
+     * Requirements:
+     * - In the returned assignments, the debaters and judges are disjoint.
+     * - Where DebaterLoadConstraint fields are present for a debater, they are obeyed (inclusive)
+     * - To the extent possible, the following are (probabilistically) spread out as much as
+     *   possible between people:
+     *   - the number of stories each person has had to read
+     *     (one must read a story to debate; cannot have read it to judge it)
+     *   - the number of times each person has debated 
+     *   - the number of times each person has judged each story
+     *     - the number of times each person has judged (less important than judging per story)
+     *   - the proportion of times each debater has been honest
+     *   - the number of times each judge has judged each debater
+     *   - the number of times each debater has debated each other
+     *
+     * @param history all existing debates, including unfinished ones
+     * @param numQuestions the number of questions that can be debated for the new story
+     * @param debaters the people who can be scheduled to judge or debate, with constraints on their load
+     * @return a list of assignments of the debaters to their roles for the new story obeying the above requirements
+     */
+  def getScheduleForNewStory(
+    history: Vector[Debate],
+    numQuestions: Int,
+    debaters: Map[String, DebaterLoadConstraint]
+  ): Vector[DebateAssignment] = ???
+
+}
