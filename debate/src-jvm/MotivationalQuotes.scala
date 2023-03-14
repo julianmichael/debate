9a5c476eedbb7c025249443cc0e1ca054828fb4c
Julian Michael <julianjohnmichael@gmail.com>
2023-03-13 21:52:50 -0400

    Add more motivational quotes

--- debate/src-jvm/MotivationalQuotes.scala
+++ debate/src-jvm/MotivationalQuotes.scala
@@ -13,7 +13,11 @@ object MotivationalQuotes {
   val yourTurn = Quotes(
     "The time is now. The time is always now.",
     "You will speak up. You will show up. You will stand up.",
-    "Let your light shine. Shine within you so that it can shine on someone else."
+    "Let your light shine. Shine within you so that it can shine on someone else.",
+    "You are not your circumstances. You are your possibilities. If you know that, you can do anything.",
+    "The great courageous act that we must all do, is to have the courage to step out of our history and past so that we can live our dreams.",
+    "The thing you fear most has no power. Your fear of it is what has the power.",
+    "Create the highest, grandest vision possible for your life because you become what you believe."
   )
 
   val newJudging = Quotes(
@@ -27,7 +31,6 @@ object MotivationalQuotes {
 
   val newMaterial = Quotes(
     "The more you know, the better you do.",
-    "You are not your circumstances. You are your possibilities. If you know that, you can do anything.",
     "Luck is preparation meeting the moment of opportunity.",
     "You face the biggest challenge of all: to have the courage to seek your big dream regardless of what anyone says."
   )
@@ -39,12 +42,15 @@ object MotivationalQuotes {
 
   val youveWon = Quotes(
     "The more you praise and celebrate your life, the more there is in life to celebrate.",
-    "You get in life what you have the courage to ask for."
+    "You get in life what you have the courage to ask for.",
+    "The harder the struggle, the more glorious the triumph.",
+    "It doesn't matter who you are, where you come from. The ability to triumph begins with you — always."
   )
 
   val youveLost = Quotes(
     "Turn your wounds into wisdom.",
-    "Failure is just an experience.",
+    "Failure is a great teacher and, if you are open to it, every mistake has a lesson to offer.",
+    "Failure is just an experience... just an experience.",
     "Sometimes winning is just finishing.",
     "Doing what is right, fair, or honorable is more important than winning or losing.",
     "The key to realizing a dream is to focus not on success but on significance—and then even the small steps and little victories along your path will take on greater meaning.",
